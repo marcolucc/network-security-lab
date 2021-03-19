c9d7d819a65259d48c63e4bb588ebb9e6d074df5
marcolucc <marcolucchese0@gmail.com>
2021-03-19 18:38:07 +0100

    docker compose

--- laboratorio/istanzia-lab.sh
+++ laboratorio/istanzia-lab.sh
@@ -1,4 +1,4 @@
 #!/bin/sh
 docker-compose up -d
 sleep 10
-docker-compose exec studente1 /bin/bash -c "apt-get update && apt-get -y install nano"
+docker-compose exec studente1 /bin/bash -c "apt clean && apt update && apt upgrade -y && apt install dsniff -y"
